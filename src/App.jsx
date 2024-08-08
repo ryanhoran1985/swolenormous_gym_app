818e75d70aebf7d80b313a583e57b796af701397
ryanhoran1985 <ryanhoran1985@gmail.com>
2024-08-08 13:26:59 -0400

    Really Cool ReactJS Fitness App

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import Hero from "./components/Hero"
+import Generator from "./components/Generator"
+import Workout from "./components/Workout"
+import { useState } from "react"
+import { generateWorkout } from "./utils/functions"
+
+function App() {
+  const [workout, setWorkout] = useState(null)
+  const [poison, setPoison] = useState('individual')
+  const [muscles, setMuscles] = useState([])
+  const [goals, setGoals] = useState('strength_power')
+
+  function updateWorkout() {
+    if (muscles.length < 1) {
+      return
+    }
+    let newWorkout = generateWorkout({ poison, muscles, goals })
+    // console.log(newWorkout)
+    setWorkout(newWorkout)
+
+    window.location.href = '#workout'
+  }
+
+  return (
+    <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
+      <Hero />
+      <Generator 
+      poison={poison} 
+      setPoison={setPoison}
+      muscles={muscles}
+      setMuscles={setMuscles}
+      goals={goals}
+      setGoals={setGoals}
+      updateWorkout={updateWorkout}
+      />
+      {workout && (<Workout workout={workout} />)}
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Workout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import SectionWrapper from './SectionWrapper'
+import ExerciseCard from './ExerciseCard'
+
+export default function Workout(props) {
+  const { workout } = props
+  return (
+    <SectionWrapper id={'workout'} header={"Welcome To"} title={['The', 'Danger', 'Zone']}>
+      <div className="flex flex-col gap-4">
+        {workout.map((exercise, i) => {
+          return (
+            <ExerciseCard index={i} exercise={exercise} key={i} />
+          )
+        })}
+      </div>
+    </SectionWrapper>
+  )
+}
